c9c0e20112c762aee31b4fed23a53b851809e44d
Edmar Radanovis <edradanovis@gmail.com>
2025-07-13 21:33:58 -0300

    Adjust next.config and force API routes as dynamic

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-	return NextResponse.json({ message: "Teste Rota OK" }, { status: 200 });
+export async function GET(req: Request) {
+	const { searchParams } = new URL(req.url);
+	const name = searchParams.get("name") || "Usuário";
+	return NextResponse.json(
+		{ message: `Teste Rota OK, ${name}!` },
+		{ status: 200 },
+	);
 }
+
+export const dynamic = "force-dynamic"; // Força como rota dinâmica
